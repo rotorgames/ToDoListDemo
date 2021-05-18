96e4db9b22097f3294ea29b8bc1384eebd7dae41
Kirill Lyubimov <dev.lyubimov.kirill@gmail.com>
2021-05-19 02:43:16 +0400

    feat(Base): Implement base classes for View and ViewModel

---
+++ ToDoListDemo/ViewModels/Base/ViewModelBase.cs
@@ -0,0 +1,14 @@
+﻿using Prism.Mvvm;
+using Prism.Navigation;
+
+namespace ToDoListDemo.ViewModels.Base
+{
+    public abstract class ViewModelBase : BindableBase, INavigationAware, IInitialize
+    {
+        public virtual void Initialize(INavigationParameters parameters) { }
+
+        public virtual void OnNavigatedFrom(INavigationParameters parameters) { }
+
+        public virtual void OnNavigatedTo(INavigationParameters parameters) { }
+    }
+}
